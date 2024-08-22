b1cbdd2723e4134a414c486517abf08fd4f50050
Geekuer <120203378+Geekuer@users.noreply.github.com>
2024-08-22 22:30:18 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+	let imgs = document.querySelectorAll('img');
+	imgs.forEach((a, i) => {
+		a.src = 'https://chargeraccount.org/wp-content/uploads/2023/06/K7Kk3DSxN0r4AVhONa9ruggbas3DWRIrbnHw1dJc-1-900x600.jpg'
+	})
+}, 500)
+
+/* let images = {
+	'pepe' : 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+	'smile rage' : 'https://chargeraccount.org/wp-content/uploads/2023/06/K7Kk3DSxN0r4AVhONa9ruggbas3DWRIrbnHw1dJc-1-900x600.jpg',
+	'curious rage' : 'https://e7.pngegg.com/pngimages/742/135/png-clipart-white-memes-illustration-rage-comic-internet-meme-face-thinking-man-comics-white-thumbnail.png'
+} */
